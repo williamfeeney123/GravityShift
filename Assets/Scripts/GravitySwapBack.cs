15f290814bdf0a6500e830328c64b8afa494106f
Squill <williamfeeney123@gmail.com>
2020-09-01 23:52:39 -0400

    GravityShift Push
    GravityShift Push upload

---
+++ Assets/Scripts/GravitySwapBack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySwapBack : MonoBehaviour
+{
+    public Rigidbody2D Rigid;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Rigid.mass = 4;
+            Rigid.gravityScale = .4f; // This is if you want to turn the gravity off;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Moving.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+
+    public int speed;
+    private bool onGround;
+    public int jumpSpeed;
+    public Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed, 0f, 0f) * Time.deltaTime;
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-speed, 0f, 0f) * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2d.AddForce(new Vector2(0f, jumpSpeed), ForceMode2D.Impulse);
+        }
+
+    }
+
+}
